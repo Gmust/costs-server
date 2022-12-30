6480d6388cc613032e913658f26b5d645f8816d7
Gmust <4orgood@gmail.com>
2022-12-30 22:10:12 +0100

    for docker

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {NestFactory} from '@nestjs/core'
 import {AppModule} from './app.module'
+import * as process from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -8,7 +9,7 @@ async function bootstrap() {
     allowedHeaders: 'Content-Type, Authorization, X-PINGOTHER, Content-Type',
     origin: 'http://127.0.0.1:5173'
   })
-  await app.listen(8080)
+  await app.listen(parseInt(process.env.PORT)  || 8080)
 }
 
 bootstrap()
